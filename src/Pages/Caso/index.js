83e9078009eb99c4cc922a19e78ba0431b6ffd52
Weslei Luiz de Paula Pinto <weslei.luiz@softwillians.com.br>
2020-03-29 04:57:00 -0300

    add pagina de detalhamento de caso e api para enviar email e whatsapp

--- src/Pages/Caso/index.js
+++ src/Pages/Caso/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { View, FlatList, Text, Image, TouchableOpacity } from 'react-native';
 import { Feather } from "@expo/vector-icons";
+import { useNavigation } from '@react-navigation/native';
 
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
 
 export default function Caso() {
+  const navigation = useNavigation();
+
+  function navigateToDetalhe() {
+    navigation.navigate('Detalhes');
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -20,83 +27,31 @@ export default function Caso() {
 
       <FlatList
         data={[1, 2, 3]}
+        keyExtractor={caso => String(caso)}
+        showsVerticalScrollIndicator={false}
+        style={styles.casos}
+        renderItem={() => (
+          <View style={styles.caso}>
+            <Text style={styles.casoProperty}>ONG:</Text>
+            <Text style={styles.casoValue}>Apad</Text>
+
+            <Text style={styles.casoProperty}>Caso:</Text>
+            <Text style={styles.casoValue}>Cadelinha lasarenta</Text>
+
+            <Text style={styles.casoProperty}>Valor:</Text>
+            <Text style={styles.casoValue}>R$ 500,0</Text>
+
+            <TouchableOpacity
+              style={styles.detailsButton}
+              onPress={navigateToDetalhe}>
+              <Text style={styles.detailsButtonText}>
+                Ver mais detalhes
+            </Text>
+              <Feather name="arrow-right" size={16} color="#E02041" />
+            </TouchableOpacity>
+          </View>
+        )}
       />
-
-      <View style={styles.casos}>
-        <View style={styles.caso}>
-          <Text style={styles.casoProperty}>ONG:</Text>
-          <Text style={styles.casoValue}>Apad</Text>
-
-          <Text style={styles.casoProperty}>Caso:</Text>
-          <Text style={styles.casoValue}>Cadelinha lasarenta</Text>
-
-          <Text style={styles.casoProperty}>Valor:</Text>
-          <Text style={styles.casoValue}>R$ 500,0</Text>
-
-          <TouchableOpacity
-            style={styles.detailsButton}
-            onPress={() => { }}>
-            <Text style={styles.criarbutton}>
-              Ver mais detalhes
-            </Text>
-            <Feather name="arrow-right" size={16} color="#E02041" />
-          </TouchableOpacity>
-        </View><View style={styles.caso}>
-          <Text style={styles.casoProperty}>ONG:</Text>
-          <Text style={styles.casoValue}>Apad</Text>
-
-          <Text style={styles.casoProperty}>Caso:</Text>
-          <Text style={styles.casoValue}>Cadelinha lasarenta</Text>
-
-          <Text style={styles.casoProperty}>Valor:</Text>
-          <Text style={styles.casoValue}>R$ 500,0</Text>
-
-          <TouchableOpacity
-            style={styles.detailsButton}
-            onPress={() => { }}>
-            <Text style={styles.criarbutton}>
-              Ver mais detalhes
-            </Text>
-            <Feather name="arrow-right" size={16} color="#E02041" />
-          </TouchableOpacity>
-        </View><View style={styles.caso}>
-          <Text style={styles.casoProperty}>ONG:</Text>
-          <Text style={styles.casoValue}>Apad</Text>
-
-          <Text style={styles.casoProperty}>Caso:</Text>
-          <Text style={styles.casoValue}>Cadelinha lasarenta</Text>
-
-          <Text style={styles.casoProperty}>Valor:</Text>
-          <Text style={styles.casoValue}>R$ 500,0</Text>
-
-          <TouchableOpacity
-            style={styles.detailsButton}
-            onPress={() => { }}>
-            <Text style={styles.criarbutton}>
-              Ver mais detalhes
-            </Text>
-            <Feather name="arrow-right" size={16} color="#E02041" />
-          </TouchableOpacity>
-        </View><View style={styles.caso}>
-          <Text style={styles.casoProperty}>ONG:</Text>
-          <Text style={styles.casoValue}>Apad</Text>
-
-          <Text style={styles.casoProperty}>Caso:</Text>
-          <Text style={styles.casoValue}>Cadelinha lasarenta</Text>
-
-          <Text style={styles.casoProperty}>Valor:</Text>
-          <Text style={styles.casoValue}>R$ 500,0</Text>
-
-          <TouchableOpacity
-            style={styles.detailsButton}
-            onPress={() => { }}>
-            <Text style={styles.detailsButtonText}>
-              Ver mais detalhes
-            </Text>
-            <Feather name="arrow-right" size={16} color="#E02041" />
-          </TouchableOpacity>
-        </View>
-      </View>
     </View>
   )
 }
--- src/Pages/Detalhe/index.js
+++ src/Pages/Detalhe/index.js
@@ -1,10 +1,76 @@
-import React from 'react'
-import View from 'react-native'
+import React from 'react';
+import { View, Image, Text, TouchableOpacity, Linking } from 'react-native';
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from '@react-navigation/native';
+import * as MailComposer from 'expo-mail-composer';
+
+import logoImg from '../../assets/logo.png';
+
+import styles from './styles';
 
 export default function Detalhe() {
-  return (
-    <View>
+  const navigation = useNavigation();
+  const message = 'VAI SEGURANDO MANO';
 
-    </View>
+  function navigateBack() {
+    navigation.navigate('Casos');
+  }
+
+  function sendMail() {
+    MailComposer.composeAsync({
+      subject: 'Herói do caso: Cadelinha atropelada',
+      recipients: ['weslei.luiz71@gmail.com'],
+      body: message
+    })
+  }
+
+  function sendWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=5512981553834&text=${message}`);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+
+        <TouchableOpacity
+          onPress={navigateBack}>
+          <Feather name="arrow-left" size={28} color="#E02041" />
+        </TouchableOpacity>
+      </View >
+
+
+      <View style={styles.caso}>
+        <Text style={[styles.casoProperty, { marginTop: 0 }]}>ONG:</Text>
+        <Text style={styles.casoValue}>Apad</Text>
+
+        <Text style={styles.casoProperty}>Caso:</Text>
+        <Text style={styles.casoValue}>Cadelinha lasarenta</Text>
+
+        <Text style={styles.casoProperty}>Valor:</Text>
+        <Text style={styles.casoValue}>R$ 500,0</Text>
+
+      </View>
+
+      <View style={styles.contactBox}>
+        <Text style={styles.heroTitle}>Salve o dia!</Text>
+        <Text style={styles.heroTitle}>Seja o heroi desse caso!</Text>
+
+        <Text style={styles.heroDescription}>Entre em contato:</Text>
+
+        <View style={styles.actions}>
+          <TouchableOpacity
+            onPress={sendWhatsapp}
+            style={styles.action}>
+            <Text style={styles.actionText}>WhatsApp</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={sendMail}
+            style={styles.action}>
+            <Text style={styles.actionText}>Email</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View >
   )
 }
